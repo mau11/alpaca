fde65d606a9e15117dc3712111b25880896db84c
Cai Lu <caillu@uw.edu>
2016-12-15 09:14:01 -0800

    (fix) Rename to AddQuiz component

--- src/pages/AddQuiz.js
+++ src/pages/AddQuiz.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-export default class ManageQuiz extends React.Component {
+export default class AddQuiz extends React.Component {
  constructor(props) {
     super(props);
 
