c035fa8051d321164d8416e72b189e808da66bd7
stomwork@gmail.com <stomwork@gmail.com>
2016-12-15 22:55:49 -0800

    Login link under 'Get started' no longer displaying when logged in

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -12,8 +12,8 @@ export default class Homepage extends React.Component {
       quizNames: []
     };
   }
-
   render() {
+    console.log(this.props.route.auth);
     return (
       <div className="container homepage">
         <div className="row text-center hero-section">
@@ -32,7 +32,7 @@ export default class Homepage extends React.Component {
           <div className="col-md-4 text-left">
               <h2>Get started</h2>
               <p>Take a quiz by clicking on "PreBuilt Quiz" in the top bar.  Or click "Custom Quiz" to start making your very own quiz for others!</p>
-              <Link to="/login">Log In</Link>
+              {!this.props.route.auth.loggedIn() ? (<Link to="/login">Log In</Link>) : ''}
           </div>
         </div>
 
