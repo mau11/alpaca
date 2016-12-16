0e3e6a3df8f22e52a95def831f70b5e8fd6d39c0
d-burke <omnidan@yahoo.com>
2016-12-15 19:25:42 -0800

    fix GameOverview divs

--- src/pages/GameOverview.js
+++ src/pages/GameOverview.js
@@ -14,13 +14,14 @@ export default class GameOverview extends React.Component {
             <div className="col-md-6">
               <div>
                 <img className="game-overview-icon" alt="Regular Quiz" src="http://s3.amazonaws.com/libapps/sites/611/icons/4179/app_store_icon.png" />
-                <Link to="/prebuiltQuiz"><h2>Regular Quiz</h2></Link></div>
+                <Link to="/prebuiltQuiz"><h2>Regular Quiz</h2></Link>
+              </div>
             </div>
           </div>
-            <div className="col-md-6">
-              <div>
-                <img className="game-overview-icon" alt="Regular Quiz" src="http://s3.amazonaws.com/libapps/sites/611/icons/4179/app_store_icon.png" />
-                <Link to="/carQuizGame"><h2>Car Quiz Game</h2></Link></div>
+          <div className="col-md-6">
+            <div>
+              <img className="game-overview-icon" alt="Regular Quiz" src="http://s3.amazonaws.com/libapps/sites/611/icons/4179/app_store_icon.png" />
+              <Link to="/carQuizGame"><h2>Car Quiz Game</h2></Link>
             </div>
           </div>
         </div>
