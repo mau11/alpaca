9aad464419e5c2e3835b1dde761f9defdf59c997
d-burke <omnidan@yahoo.com>
2016-12-15 19:01:47 -0800

    first version of car game

--- src/pages/GameOverview.js
+++ src/pages/GameOverview.js
@@ -8,7 +8,18 @@ export default class GameOverview extends React.Component {
 
   render() {
     return (
-      <div><Link to="/prebuiltQuiz">Regular Quiz</Link></div>
+      <div>
+        <ul className="list-group row">
+          <li className="list-group-item col-xs-6">
+            <img className="game-overview-icon" alt="Regular Quiz" src="http://s3.amazonaws.com/libapps/sites/611/icons/4179/app_store_icon.png" />
+            <Link to="/prebuiltQuiz">Regular Quiz</Link>
+          </li>
+          <li className="list-group-item col-xs-6">
+            <img className="game-overview-icon" alt="Car Quiz" src="http://s3.amazonaws.com/libapps/sites/611/icons/4179/app_store_icon.png" />
+            <Link to="/carQuizGame">Car Quiz Game</Link>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
