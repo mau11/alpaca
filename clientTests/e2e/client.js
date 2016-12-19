f428364f3ca6fb79d6a28e7c8ee3339097c2b746
Maureen E. Mirville <mirvil7130@aol.com>
2016-12-19 04:26:44 -0800

    added client side tests

--- clientTests/e2e/client.js
+++ clientTests/e2e/client.js
@@ -1,26 +1,43 @@
-describe('Grid test', function() {
+// To run these tests, enter the following in your terminal.
+// $ npm run client
 
-  before(function(client, done) {
-    done();
-  });
+describe('Crash Course (before logging in)', function() {
 
-  afterEach(function(client, done) {
-    done();
-  });
-
-  beforeEach(function(client, done) {
-    done();
-  });
-
-  it('Test Google', function (browser) {
-    browser
-      .url('http://www.google.com/ncr')
+  it('should load page', function (client, done) {
+    client
+      .url('http://localhost:1337')
       .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatchjs')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .assert.title('CrashCourse')
+      .assert.visible('.navbar-brand')
+      .assert.visible('.fa.fa-desktop')
+      .assert.visible('#volumeIcon')
       .pause(1000);
     });
+
+  it('should prompt auth0 when clicking log in', function(client){
+    client
+      .assert.visible('.login')
+      .click('.login')
+      .waitForElementVisible('.auth0-lock-header-logo', 1000)
+      .click('.auth0-lock-close-button')
+      .pause(1000);
+  });
+
+  it('should load all games without logging in', function(client){
+    client
+      client.click('link text', 'Games')
+      .pause(1000);
+      client.click('link text', 'Regular Quiz Game')
+      .pause(1000);
+      client.click('link text', 'Car Quiz')
+      .pause(1000);
+      client.click('link text', 'Racer Quiz Game')
+      .pause(1000);
+      client.click('link text', '3D Racer Quiz Game')
+      .pause(1000);
+      client.click('link text', 'Wolfenstein 3D - Quizz edition')
+      .pause(1000);
+  });
 
   after(function(client, done) {
     client.end(function() {
@@ -28,3 +45,25 @@ describe('Grid test', function() {
     });
   });
 });
+
+
+describe('Crash Course (after logging in)', function() {
+
+  it('should have access to all management components of app after logging in', function(client){
+    client
+    .url('http://localhost:1337/#/access_token=AD06oaKJ1Q3QAKIU&expires_in=86400&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3N0ZWZhbnIuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTExODMwMTI1NDM5MDYwNDcyNDE5IiwiYXVkIjoiaUg3SHZ4cTdHa2d4WkVJVkZLN050YjV5U21UOGpXZEUiLCJleHAiOjE0ODIxODIzNjIsImlhdCI6MTQ4MjE0NjM2Mn0.r8l-ZybZz-MBHGHIm1hD5fvfuf3RBWStw4FyBUBABRw&token_type=Bearer')
+    .waitForElementVisible('body', 5000)
+    .click('link text', 'Build a Quiz')
+/*    .click('link text', 'Manage Quizzes')
+    .click('link text', 'My Results')
+    .click('link text', 'Log Out')*/
+    .pause(1000);
+  });
+
+  after(function(client, done) {
+    client.end(function() {
+      done();
+    });
+  });
+});
+
