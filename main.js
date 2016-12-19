6eecbd922ebfd9f1a00336514c6d7c64471b58f0
Cai Lu <caillu@uw.edu>
2016-12-19 14:33:53 -0800

    (cleanup) Remove default menus, add productName

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const electron = require('electron');
 const Menu = electron.Menu;
-const MenuItem = electron.MenuItem;
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
@@ -9,195 +8,86 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+// The menu bar template
 const template = [
   {
-    label: 'Games',
-    submenu: [{
-    label: 'Regular Quiz',
-    click: function() {
-      mainWindow.loadURL('http://localhost:1337/#/prebuiltQuiz');
-    }
-  },
-  {
-    label: 'Car Quiz',
-    click: function() {
-      mainWindow.loadURL('http://localhost:1337/#/carQuizGame');
-    }
-  },
-  {
-    label: 'Racer Quiz',
-    click: function() {
-      mainWindow.loadURL('http://localhost:1337/game/v4.final.html');
-    }
-  },
-  {
-    label: '3D Racer Quiz',
-    click: function() {
-      mainWindow.loadURL('http://localhost:1337/hex/index.html');
-    }
-  },
-  {
-    label: 'Wolfenstein 3D',
-    click: function() {
-      mainWindow.loadURL('http://localhost:1337/wolf/index.html');
-    }
-  }]
-  },
-  {
-    label: 'View',
+    label: 'Cats', // This isn't working as expected
     submenu: [
-      {
-        role: 'reload'
-      },
-      {
-        role: 'toggledevtools'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'resetzoom'
-      },
-      {
-        role: 'zoomin'
-      },
-      {
-        role: 'zoomout'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen'
-      }
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {
-        role: 'minimize'
-      },
-      {
-        role: 'close'
-      }
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click () { require('electron').shell.openExternal('http://electron.atom.io') }
-      }
-    ]
-  }
-]
-
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {
-        role: 'about'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'services',
-        submenu: []
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'hide'
-      },
-      {
-        role: 'hideothers'
-      },
-      {
-        role: 'unhide'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'quit'
-      }
-    ]
-  })
-  // Window menu.
-  template[3].submenu = [
     {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    },
-    {
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    },
-    {
-      label: 'Zoom',
-      role: 'zoom'
+      role: 'about'
     },
     {
       type: 'separator'
     },
     {
-      label: 'Bring All to Front',
-      role: 'front'
-    }
-  ]
-}
+      role: 'quit'
+    }]
+  },
+  {
+    label: 'Games',
+    submenu: [
+    {
+      label: 'Regular Quiz',
+      click: function() {
+        mainWindow.loadURL('http://localhost:1337/#/prebuiltQuiz');
+      }
+    },
+    {
+      label: 'Car Quiz',
+      click: function() {
+        mainWindow.loadURL('http://localhost:1337/#/carQuizGame');
+      }
+    },
+    {
+      label: 'Racer Quiz',
+      click: function() {
+        mainWindow.loadURL('http://localhost:1337/game/v4.final.html');
+      }
+    },
+    {
+      label: '3D Racer Quiz',
+      click: function() {
+        mainWindow.loadURL('http://localhost:1337/hex/index.html');
+      }
+    },
+    {
+      label: 'Wolfenstein 3D',
+      click: function() {
+        mainWindow.loadURL('http://localhost:1337/wolf/index.html');
+      }
+    }]
+  }
+];
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1100, height: 750});
 
-
-  // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:1337/');
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
+
+  console.log(process.platform);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', function() {
   createWindow();
 
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
